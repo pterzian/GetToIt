0674f5d64daa40ed575d5d27c8d102af5e745559
pterzian <pl.terzian@gmail.com>
2017-04-11 22:50:48 +0200

    adding noNames

--- R/trybox.R
+++ R/trybox.R
@@ -7,7 +7,8 @@
 #'
 #'@param listOflist A list of lists, possibly named
 #'@param newOrder An index of the lists in the order you want them to be plot
-#'
+#'@param noNames if no names given they are generated
+#'@param df Yes, you can give a data-frame...
 #'
 #'@examples
 #'magic <- list(list(c(rnorm(20,2,1)), c(rnorm(8,8,1))), list(c(rnorm(20,3,1)), c(rnorm(8,7,1))), list(c(rnorm(20,4,1)), c(rnorm(8,6,1))))
@@ -20,30 +21,40 @@
 
 
 
-ggBoxPlot <-function(listOflist, newOrder) {
+ggBoxPlot <-function(listOflist, newOrder, noNames=FALSE, df = FALSE) {
 
-  boxNames       <- names(listOflist)
-
-  foo = lapply(seq_along(listOflist), function(i) {
-
-    currentBox   <- listOflist[[i]]
-
-    ##get p-value from wilcox.test
-    if(all(is.na(unlist(currentBox)))){
-      return(NA)
+  if(df){
+    bigDF <- listOflist
+  }
+  else{
+    if(!noNames){
+      boxNames     <- names(listOflist)
     }
     else{
-
-      dfelectro  <- data.frame(value = unlist(currentBox[[1]]), type = "electro", group = boxNames[i], stringsAsFactors = FALSE)
-      dfmetel    <- data.frame(value = unlist(currentBox[[2]]), type = "metal", group = boxNames[i], stringsAsFactors = FALSE)
-      dfTot      <- rbind(dfelectro, dfmetel)
-
-      return(dfTot)
+      names(listOflist) <- c(1:length(listOflist))
     }
-  })
 
-  bigDF          <- do.call(rbind, foo)
 
+    foo = lapply(seq_along(listOflist), function(i) {
+
+      currentBox   <- listOflist[[i]]
+      if(all(is.na(unlist(currentBox)))){
+        return(NA)
+      }
+      else{
+
+        dfelectro <- data.frame(value = unlist(currentBox[[1]]), type = "electro", group = boxNames[i], stringsAsFactors = FALSE)
+        dfmetel   <- data.frame(value = unlist(currentBox[[2]]), type = "metal", group = boxNames[i], stringsAsFactors = FALSE)
+        dfTot     <- rbind(dfelectro, dfmetel)
+
+        return(dfTot)
+      }
+    })
+
+    bigDF        <- do.call(rbind, foo)
+  }
+
+  ################################### Now we get the right order
 
   tmp            <- factor(bigDF$group, levels = newOrder, ordered = TRUE)
   bigDF          <- bigDF[order(tmp),]
@@ -56,8 +67,8 @@ ggBoxPlot <-function(listOflist, newOrder) {
   bigDF$newGroup <- paste(vecRep, bigDF$group, sep=".")
 
   ProbesBoxplot  <- ggplot2::ggplot(data= bigDF, ggplot2::aes(x = newGroup, y = value))  +
-                    ggplot2::geom_boxplot((ggplot2::aes(fill=type))) +
-                    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
+    ggplot2::geom_boxplot((ggplot2::aes(fill=type))) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
 
   return(list(ProbesBoxplot, bigDF))
 
