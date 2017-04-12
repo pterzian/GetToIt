c35ab87c010a8163d6b3e6ca055fc4822832aee7
pterzian <pl.terzian@gmail.com>
2017-04-12 19:34:02 +0200

    orderd by factor

--- R/trybox.R
+++ R/trybox.R
@@ -5,7 +5,7 @@
 #'
 #'
 #'
-#'@param listOflist A list of lists, possibly named
+#'@param listOflist A list of lists, possibly named. The sub-lists must have 2 vectors of values to be boxplotted, each list must contain the same amount of values for now.
 #'@param newOrder An index of the lists in the order you want them to be plot
 #'@param noNames if no names given they are generated
 #'@param df Yes, you can give a data-frame...
@@ -13,7 +13,7 @@
 #'@examples
 #'magic <- list(list(c(rnorm(20,2,1)), c(rnorm(8,8,1))), list(c(rnorm(20,3,1)), c(rnorm(8,7,1))), list(c(rnorm(20,4,1)), c(rnorm(8,6,1))))
 #'names(magic) <- c(1:length(magic))
-#'order= c("2","3", "1")
+#'order= c(1,3,2)
 #'p <- ggBoxPlot(magic, order)
 #'
 #'@export
@@ -51,22 +51,17 @@ ggBoxPlot <-function(listOflist, newOrder, noNames=FALSE, df = FALSE) {
       }
     })
 
-    bigDF        <- do.call(rbind, foo)
+    bigDF         <- do.call(rbind, foo)
   }
 
   ################################### Now we get the right order
+  tmp             <- factor(bigDF$group, levels = newOrder, ordered = TRUE)
+  bigDF           <- bigDF[order(tmp),]
 
-  tmp            <- factor(bigDF$group, levels = newOrder, ordered = TRUE)
-  bigDF          <- bigDF[order(tmp),]
+  ind             <- apply(bigDF, 1, function(x) all(is.na(x)))
+  bigDF           <- bigDF[!ind, ]
 
-  ind            <- apply(bigDF, 1, function(x) all(is.na(x)))
-  bigDF          <- bigDF[!ind, ]
-
-  vec            <- length(unique(bigDF$group))
-  vecRep         <- rep(LETTERS[1:vec], each= length(bigDF$group) / vec)
-  bigDF$newGroup <- paste(vecRep, bigDF$group, sep=".")
-
-  ProbesBoxplot  <- ggplot2::ggplot(data= bigDF, ggplot2::aes(x = newGroup, y = value))  +
+  ProbesBoxplot   <- ggplot2::ggplot(data= bigDF, ggplot2::aes(x = factor(group, levels = unique(group)), y = value))  +
     ggplot2::geom_boxplot((ggplot2::aes(fill=type))) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
 
